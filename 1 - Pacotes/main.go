e182a895150c4c3c9b6feed3eb02d6345641026d
vitindev <victtorhugo.mendes@gmail.com>
2023-10-20 00:55:44 -0300

    anotação

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -5,6 +5,16 @@ import (
 	"pacotes/auxiliar"
 )
 
+/*
+
+	go mod tidy - Remove dependencias externas que não estão sendo utilizadas.
+	go get (url) - Adiciona dependencias externas no go.mod para serem utilizados.
+	go build - Compila a aplicação.
+	go run (arquivo.go) - Iniciar uma aplicação go
+	go mod init (nome do mod)
+
+*/
+
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
